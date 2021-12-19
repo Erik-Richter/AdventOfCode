15b2b9da02e0191f69c5b49e77358039bc38244f
Erik-Richter <richtererik20@gmail.com>
2021-12-19 14:46:53 +0100

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,61 +6,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Point> board = new ArrayList<>();
-        List<Line> mainList = loadData();
-        drawLinesToBoard(mainList, board);
-
-        int counter = 0;
-        for (Point point:board)
-            if(point.getOccurrence() > 1)
-                counter += 1;
-
-        System.out.println(counter);
-    }
-
-    public static void drawLinesToBoard(List<Line> lines, List<Point> board){
-        for(Line line:lines){
-            for(int i = 0; i < line.getPoints().size(); i++){
-                Point actualPoint = line.getPoints().get(i);
-                if(board.contains(actualPoint)){
-                    int newOccurrence = actualPoint.getOccurrence() +1;
-                    int idxBoard = board.indexOf(actualPoint);
-                    if(idxBoard != -1)
-                        board.get(idxBoard).setOccurrence(newOccurrence);
-                } else {
-                    actualPoint.setOccurrence(1);
-                    board.add(actualPoint);
-                }
-            }
-        }
-    }
-
-    public static List<Line> loadData(){
-
-        String str1;
-        int x1, x2, y1, y2;
-        List<Line> mainList = new ArrayList<>();
-
-        try{
-            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/data/coordinates.txt"));
-            while ((str1 = br.readLine()) != null){
-                //System.out.println(str1);
-                String[] parsedStrings = str1.split("->");
-                String[] a = parsedStrings[0].trim().split(",");
-                String[] b = parsedStrings[1].trim().split(",");
-
-                x1 = Integer.parseInt(a[0]);
-                y1 = Integer.parseInt(a[1]);
-                x2 = Integer.parseInt(b[0]);
-                y2 = Integer.parseInt(b[1]);
-
-
-                mainList.add(new Line(x1, y1, x2, y2));
-            }
-            br.close();
-        }catch (Exception e){
-            throw new IllegalStateException("loadData exception");
-        }
-        return mainList;
+        System.out.println("ready2Day06");
     }
 }
